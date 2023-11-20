7a7505bec0d274a7c0f61b3c561d70a6415f60a1
sobcode <116833102+sobcode@users.noreply.github.com>
2023-11-20 07:32:55 +0200

    Add files via upload

---
+++ quadratic-equation/src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -0,0 +1,27 @@
+package com.epam.rd.autotasks;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double d = Math.pow(b, 2) - 4 * a * c;
+        if(d < 0){
+            System.out.println("no roots");
+        } else if(d == 0){
+            System.out.println(-b / (2 * a));
+        } else{
+            double x1 = (-b - Math.sqrt(d)) / (2 * a);
+            double x2 = (-b + Math.sqrt(d)) / (2 * a);
+            System.out.println(x1 + " " + x2);
+        }
+    }
+
+}
